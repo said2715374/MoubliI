9d0b6a521fff0c35fde988d3f926ac95df8006de
said2715374 <2715374@gmail.com>
2023-11-28 17:03:10 -0500

    Création d'une condition sur le login
    création des condition sur le USERNAME et le PASSWORD c'est les condition ne sont pas respecter il vas envoyer une alerte si non il vas nous envoyer sur la page d'accueil

--- js/login.js
+++ js/login.js
@@ -1,1 +1,26 @@
-console.log("hello")
+document.addEventListener('DOMContentLoaded', function() {
+  const submitButton = document.getElementById('submitButton');
+  const loginForm = document.getElementById('loginForm');
+
+  submitButton.addEventListener('click', function(event) {
+    event.preventDefault();
+
+
+    const username = document.getElementById('usernameInput').value.trim();
+    const password = document.getElementById('passwordInput').value.trim();
+
+   
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d!@#$%^&*()_+]{8,}$/;
+
+
+    if (username === '' || password === '') {
+      alert('Veuillez saisir votre nom dutilisateur et votre mot de passe');
+    } else if (!passwordRegex.test(password)) {
+      alert('Le mot de passe doit contenir au moins 8 caractères, dont des majuscules, des minuscules, des chiffres et des symboles.');
+    } else {
+
+      loginForm.submit();
+      window.location.href = 'index.html';
+    }
+  });
+});
